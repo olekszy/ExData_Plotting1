6ef34282160def42f98d4c3a43f359ebf255bd0b
olekszy <57833828+olekszy@users.noreply.github.com>
2020-07-13 13:28:18 +0200

    Create plot01.R

---
+++ plot01.R
@@ -0,0 +1,17 @@
+
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+df$Date <- as.Date(df$Date, "%d/%m/%Y") #Type Date
+df <- subset(df,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2")) #filtering according to instructions
+df <- df[complete.cases(df),] #remove incomplete
+dateTime <- paste(df$Date, df$Time) #bind two columns
+dateTime <- setNames(dateTime, "DateTime")
+df <- df[ ,!(names(df) %in% c("Date","Time"))]
+
+df <- cbind(dateTime, df) #adding dateTime
+df$dateTime <- as.POSIXct(dateTime)
+
+#First pLot
+hist(df$Global_active_power, 
+     main="Global_Active_Power",  
+     xlab = "Global_Active_Power_(kW)",
+     col="purple")
